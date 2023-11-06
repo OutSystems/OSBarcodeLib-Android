9948474a0a227574685c0f3e8faea9ab9649c8d6
Alexandre Jacinto <alexandre.jacinto@outsystems.com>
2023-11-06 16:41:55 +0000

    Finish initialisation by running the generator script.

--- src/androidTest/java/com.outsystems.plugins.barcode/osbarcodelib/ExampleInstrumentedTest.kt
+++ src/androidTest/java/com.outsystems.plugins.barcode/osbarcodelib/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package organizationidplaceholder.libtemplateplaceholder
+package com.outsystems.plugins.barcode.osbarcodelib
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("organizationidplaceholder.libtemplateplaceholder", appContext.packageName)
+        assertEquals("com.outsystems.plugins.barcode.osbarcodelib", appContext.packageName)
     }
 }
