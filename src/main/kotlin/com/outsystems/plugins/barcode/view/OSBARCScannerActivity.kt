58c279220c83873173bb6b0a5c43028d806e3321
Alexandre Jacinto <alexandre.jacinto@outsystems.com>
2023-11-09 19:29:33 +0000

    feat: first version of AlertDialog for app's permissions
    References: https://outsystemsrd.atlassian.net/browse/RMET-2895

--- src/main/kotlin/com/outsystems/plugins/barcode/view/OSBARCScannerActivity.kt
+++ src/main/kotlin/com/outsystems/plugins/barcode/view/OSBARCScannerActivity.kt
@@ -2,6 +2,7 @@ package com.outsystems.plugins.barcode.view
 
 import android.Manifest
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
@@ -22,9 +23,15 @@ import androidx.camera.core.ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST
 import androidx.compose.ui.Modifier
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.ContextCompat
 import com.outsystems.plugins.barcode.controller.OSBARCBarcodeAnalyzer
@@ -70,6 +77,7 @@ class OSBARCScannerActivity : ComponentActivity() {
     fun ScanScreen() {
         val lifecycleOwner = LocalLifecycleOwner.current
         val context = LocalContext.current
+        var permissionGiven by remember { mutableStateOf(true) }
 
         // permissions
         val requestPermissionLauncher = rememberLauncherForActivityResult(
@@ -77,13 +85,29 @@ class OSBARCScannerActivity : ComponentActivity() {
         ) { isGranted: Boolean ->
             if (isGranted) {
                 // do nothing, continue
+                permissionGiven = true
             } else {
-                this.setResult(OSBARCError.CAMERA_PERMISSION_DENIED_ERROR.code)
-                this.finish()
+                //this.setResult(OSBARCError.CAMERA_PERMISSION_DENIED_ERROR.code)
+                //this.finish()
+                val s = ""
+                permissionGiven = false
             }
         }
         SideEffect {
             requestPermissionLauncher.launch(Manifest.permission.CAMERA)
+        }
+
+        if (!permissionGiven) {
+            AlertDialogExample(
+                onDismissRequest = {
+                    val s = "s"
+                },
+                onConfirmation = {
+                    val s = ""
+                },
+                dialogTitle = "Camera Access Not Enabled",
+                dialogText = "To continue, please go to the Settings app and enable it."
+            )
         }
 
         // rest of the UI
@@ -144,6 +168,60 @@ class OSBARCScannerActivity : ComponentActivity() {
                     previewView
                 },
                 modifier = Modifier.weight(1f)
+            )
+        }
+    }
+
+    @Composable
+    fun AlertDialogExample(
+        onDismissRequest: () -> Unit,
+        onConfirmation: () -> Unit,
+        dialogTitle: String,
+        dialogText: String,
+    ) {
+        var dialogOpen by remember { mutableStateOf(true) }
+        val context = LocalContext.current
+
+        if (dialogOpen) {
+            AlertDialog(
+                title = {
+                    Text(
+                        text = dialogTitle,
+                        fontSize = 20.sp
+                    )
+                },
+                text = {
+                    Text(text = dialogText)
+                },
+                onDismissRequest = {
+                    //onDismissRequest()
+                    dialogOpen = false
+                },
+                confirmButton = {
+                    TextButton(
+                        onClick = {
+                            //onConfirmation()
+                            dialogOpen = false
+                            val intent = Intent().apply {
+                                action = android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+                                data = Uri.fromParts("package", context.packageName, null)
+                            }
+                            context.startActivity(intent)
+                        }
+                    ) {
+                        Text("Settings")
+                    }
+                },
+                dismissButton = {
+                    TextButton(
+                        onClick = {
+                            //onDismissRequest()
+                            dialogOpen = false
+                        }
+                    ) {
+                        Text("Ok")
+                    }
+                }
             )
         }
     }
