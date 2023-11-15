dda6fbb5dd3bf0b84bdac531168906f7f729318a
Alexandre Jacinto <alexandre.jacinto@outsystems.com>
2023-11-15 10:29:42 +0000

    feat: show scan instructions if they exist
    Context: If there are scan instructions in the scan parameters, we present them in a Text composable (https://developer.android.com/jetpack/compose/text).
    
    References: https://outsystemsrd.atlassian.net/browse/RMET-2761

--- src/main/kotlin/com/outsystems/plugins/barcode/view/OSBARCScannerActivity.kt
+++ src/main/kotlin/com/outsystems/plugins/barcode/view/OSBARCScannerActivity.kt
@@ -25,12 +25,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
@@ -204,6 +206,14 @@ class OSBARCScannerActivity : ComponentActivity() {
 
             if (camera.cameraInfo.hasFlashUnit()) {
                 TorchButton()
+            }
+
+            if (!parameters.scanInstructions.isNullOrEmpty()) {
+                Text(
+                    text = parameters.scanInstructions,
+                    modifier = Modifier.align(Alignment.Center),
+                    color = Color.White
+                )
             }
         }
     }
