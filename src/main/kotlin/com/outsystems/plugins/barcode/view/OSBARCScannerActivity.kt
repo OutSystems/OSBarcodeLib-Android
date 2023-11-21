2c038ce3322a9908532379c802bb7467d6e2f9dc
Alexandre Jacinto <alexandre.jacinto@outsystems.com>
2023-11-21 14:37:37 +0000

    feat: first version of scan frame
    References: https://outsystemsrd.atlassian.net/browse/RMET-2770

--- src/main/kotlin/com/outsystems/plugins/barcode/view/OSBARCScannerActivity.kt
+++ src/main/kotlin/com/outsystems/plugins/barcode/view/OSBARCScannerActivity.kt
@@ -22,11 +22,22 @@ import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.paddingFromBaseline
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
@@ -41,10 +52,21 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Rect
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.ClipOp
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.graphics.drawscope.clipPath
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextMeasurer
+import androidx.compose.ui.text.drawText
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
@@ -183,7 +205,8 @@ class OSBARCScannerActivity : ComponentActivity() {
         }
 
         Box(
-            modifier = Modifier.fillMaxSize()
+            modifier = Modifier
+                .fillMaxSize(),
         ) {
             AndroidView(
                 factory = { context ->
@@ -226,17 +249,93 @@ class OSBARCScannerActivity : ComponentActivity() {
                 modifier = Modifier.fillMaxSize()
             )
 
+            /*
+            val configuration = LocalConfiguration.current
+            val screenHeight = configuration.screenHeightDp.dp
+            val screenWidth = configuration.screenWidthDp.dp
+
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(Color.Black.copy(alpha = 0.6f)),
+                verticalArrangement = Arrangement.Center
+            ) {
+
+                // text with scan instructions
+                if (!parameters.scanInstructions.isNullOrEmpty()) {
+                    ScanInstructions(modifier = Modifier
+                        .align(Alignment.CenterHorizontally)
+                        //.padding(bottom = 64.dp)
+                        ,scanInstructions = parameters.scanInstructions)
+                }
+
+                // create the semi-transparent effect
+                Canvas(
+                    modifier = Modifier
+                        //.fillMaxSize(),
+                        .fillMaxWidth()
+                        .height(screenHeight / 1.3f),
+                    onDraw = {
+                    val circlePath = Path().apply {
+                        addRect(
+                            Rect(center, size.width / 4)
+                        )
+                    }
+                    clipPath(circlePath, clipOp = ClipOp.Difference) {
+                        drawRect(SolidColor(Color.Black.copy(alpha = 0.6f)))
+                    }
+                })
+
+            }
+
+             */
+
+            // create the semi-transparent effect
+            Canvas(modifier = Modifier.fillMaxSize(), onDraw = {
+                val circlePath = Path().apply {
+                    addRect(
+                        Rect(center, size.minDimension / 4)
+                    )
+                }
+                clipPath(circlePath, clipOp = ClipOp.Difference) {
+                    drawRect(SolidColor(Color.Black.copy(alpha = 0.6f)))
+                }
+            })
+
+            Column(
+                modifier = Modifier
+                    .fillMaxSize(),
+                verticalArrangement = Arrangement.Center
+            ) {
+
+                // text with scan instructions
+                if (!parameters.scanInstructions.isNullOrEmpty()) {
+                    ScanInstructions(modifier = Modifier
+                        .align(Alignment.CenterHorizontally)
+                        .padding(bottom = 72.dp)
+                        ,scanInstructions = parameters.scanInstructions)
+                }
+
+                val configuration = LocalConfiguration.current
+                val screenHeight = configuration.screenHeightDp.dp
+                val screenWidth = configuration.screenWidthDp.dp
+
+                Box(
+                    modifier = Modifier
+                        .size(screenWidth / 2)
+                        .background(Color.Transparent)
+                        .align(Alignment.CenterHorizontally)
+                ) {
+
+                }
+            }
+
             // close button
             CloseButton(
                 modifier = Modifier
                     .align(Alignment.TopEnd)
                     .padding(top = 16.dp, end = 16.dp)
             )
-
-            // text with scan instructions
-            if (!parameters.scanInstructions.isNullOrEmpty()) {
-                ScanInstructions(modifier = Modifier.align(Alignment.Center), scanInstructions = parameters.scanInstructions)
-            }
 
             Box(
                 modifier = Modifier
