ede5289eeaad8602530c46bc8f8c64fbeb286f4c
Alexandre Jacinto <alexandre.jacinto@outsystems.com>
2024-04-30 12:06:33 +0100

    feat: use CameraX method to convert image to bitmap
    Context: As we'll be able to use version 1.3.0 of the CameraX library, we can now use this the ImageProxy.toBitmap() method instead of using our own method.
    
    References: https://outsystemsrd.atlassian.net/browse/RMET-3399

--- src/main/kotlin/com/outsystems/plugins/barcode/controller/OSBARCBarcodeAnalyzer.kt
+++ src/main/kotlin/com/outsystems/plugins/barcode/controller/OSBARCBarcodeAnalyzer.kt
@@ -42,7 +42,7 @@ class OSBARCBarcodeAnalyzer(
         try {
             scanLibrary.scanBarcode(
                 image,
-                cropBitmap(imageProxyToBitmap(image)),
+                cropBitmap(image.toBitmap()),
                 {
                     onBarcodeScanned(it)
                 },
@@ -56,52 +56,6 @@ class OSBARCBarcodeAnalyzer(
         }
         image.close()
     }
-
-    /**
-     * Converts an ImageProxy object to a Bitmap.
-     * Once we can compile this library with Kotlin 1.9.10, and consequently
-     * can use version 1.5.3 of the Compose Compiler, this method is unnecessary,
-     * since we will be able to use version 1.3.0 of the CameraX library
-     * and obtain the bitmap directly from the ImageProxy, using ImageProxy.toBitmap.
-     * More info:
-     * - https://developer.android.com/jetpack/androidx/releases/compose-kotlin
-     * - https://developer.android.com/jetpack/androidx/releases/camera#1.3.0
-     * @param image - ImageProxy object that represents the image to be analyzed.
-     */
-    private fun imageProxyToBitmap(image: ImageProxy): Bitmap {
-
-        // get image data
-        val planes = image.planes
-        val yBuffer: ByteBuffer = planes[0].buffer
-        val uBuffer: ByteBuffer = planes[1].buffer
-        val vBuffer: ByteBuffer = planes[2].buffer
-
-        // get image width and height
-        val imageWidth = image.width
-        val imageHeight = image.height
-
-        // calculate image data size
-        val ySize = yBuffer.remaining()
-        val uSize = uBuffer.remaining()
-        val vSize = vBuffer.remaining()
-
-        // use byte arrays for image data
-        val data = ByteArray(ySize + uSize + vSize)
-        yBuffer.get(data, 0, ySize)
-        uBuffer.get(data, ySize, uSize)
-        vBuffer.get(data, ySize + uSize, vSize)
-
-        // create a YUV image
-        // ImageFormat.NV21 used because it's efficient and widely supported
-        val yuvImage = YuvImage(data, ImageFormat.NV21, imageWidth, imageHeight, null)
-
-        // convert YUV to Bitmap
-        val out = ByteArrayOutputStream()
-        yuvImage.compressToJpeg(Rect(0, 0, imageWidth, imageHeight), 100, out)
-        val imageBytes = out.toByteArray()
-        return imageHelper.bitmapFromImageBytes(imageBytes)
-    }
-
 
     /**
      * Creates a cropped bitmap for the region of interest to scan,
