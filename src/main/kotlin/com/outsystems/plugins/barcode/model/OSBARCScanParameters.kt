2fbd79b3e9196937eda6839f58ec87685e04ae43
Alexandre Jacinto <alexandre.jacinto@outsystems.com>
2024-05-21 15:37:54 +0100

    fix: add SeralizedName annotation to because of code obfuscation
    Context: When using code obfuscation, we need the use the SeralizedName annotation so that this object is correctly parsed.
    
    References: https://outsystemsrd.atlassian.net/browse/RMET-3394

--- src/main/kotlin/com/outsystems/plugins/barcode/model/OSBARCScanParameters.kt
+++ src/main/kotlin/com/outsystems/plugins/barcode/model/OSBARCScanParameters.kt
@@ -1,16 +1,17 @@
 package com.outsystems.plugins.barcode.model
 
 import java.io.Serializable
+import com.google.gson.annotations.SerializedName
 
 /**
  * Data class that represents the object with the scan parameters.
  */
 data class OSBARCScanParameters(
-    val scanInstructions: String?,
-    val cameraDirection: Int?,
-    val scanOrientation: Int?,
-    val scanButton: Boolean,
-    val scanText: String,
-    val hint: Int?,
-    val androidScanningLibrary: String?
+    @SerializedName("scanInstructions") val scanInstructions: String?,
+    @SerializedName("cameraDirection") val cameraDirection: Int?,
+    @SerializedName("scanOrientation") val scanOrientation: Int?,
+    @SerializedName("scanButton") val scanButton: Boolean,
+    @SerializedName("scanText") val scanText: String,
+    @SerializedName("hint") val hint: Int?,
+    @SerializedName("androidScanningLibrary") val androidScanningLibrary: String?
 ) : Serializable
